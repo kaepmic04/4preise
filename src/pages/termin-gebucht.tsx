a8f1b8ffad125139710052b44b5932b4bee93121
kaepmic04 <michel.mika04@gmail.com>
2025-03-27 14:34:35 +0100

    termingebuchtseite

---
+++ src/pages/termin-gebucht.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TerminGebucht = () => {
+  return (
+    <main style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Vielen Dank für Ihre Buchung!</h1>
+      <p>Ihr Termin wurde erfolgreich eingetragen. Wir freuen uns auf Sie.</p>
+    </main>
+  );
+};
+
+export default TerminGebucht;
